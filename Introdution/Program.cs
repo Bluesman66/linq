e7b2bf54d90db4cf1fe7c6d8461025857c2a27c2
alex.paclin <alex.paclin@gmail.com>
2019-01-18 12:51:08 +0500

    ShowLargeFileWithLinq

--- Introdution/Program.cs
+++ Introdution/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Introdution
 {
@@ -10,6 +11,8 @@ namespace Introdution
         {
             var path = @"C:\Windows";
             ShowLargeFileWithoutLinq(path);
+            Console.WriteLine("*****");
+            ShowLargeFileWithLinq(path);
             Console.ReadKey();
         }
 
@@ -29,6 +32,22 @@ namespace Introdution
             {
                 var file = files[i];
                 Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+
+        static void ShowLargeFileWithLinq(string path)
+        {
+            //var query = from file in new DirectoryInfo(path).GetFiles()
+            //            orderby file.Length descending
+            //            select file;
+
+            var query = new DirectoryInfo(path).GetFiles()                        
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
             }
         }
     }
